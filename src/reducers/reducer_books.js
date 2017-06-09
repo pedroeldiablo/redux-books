a2510b79da15ac15ab203225dcf1a4b71e760d0d
Peter Williams <pedroeldiablo@yahoo.com>
2017-06-09 15:25:26 +0100

    Adds components actions and reducers to app

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Capital', pages: 101 },
+    {title: 'JavaScript and JQuery', pages: 11},
+    {title: 'HTML and CSS', pages: 123 },
+    {title: 'Learn to Program Ruby', pages: 141},
+    {title: 'London\' Lost Tubes', pages: 101}
+  ]
+}
